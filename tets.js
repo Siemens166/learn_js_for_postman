56e05c9238e0d9cd2d808f52af3fa3fc2fb889f1
Siemens166 <mr.siemens@ya.ru>
2022-07-30 03:59:09 +0400

    Add files via upload

---
+++ tets.js
@@ -0,0 +1,149 @@
+//Получите значение "Зендея":
+
+/* spider= {
+	name: "Человек-Паук: Вдали от дома",
+	year: 2019,
+	actors: ["Том Холланд", "Зендея", "Энгаури Райс", "Джон Фавро", "Джейк Джилленхол", "Сэмюэл Л. Джексон"]
+	};
+	console.log(spider.actors[1]); */
+
+//Получите имя ее актрисы (Гвинет Пэлтроу). 
+
+/* 	marvel = [{
+		name: "Человек-Паук: Вдали от дома",
+		year: 2019,
+		actors: ["Том Холланд", "Зендея", "Энгаури Райс", "Джон Фавро", "Джейк Джилленхол", "Сэмюэл Л. Джексон"]
+		},
+		
+		{
+		name: "Мстители: Финал",
+		year: 2019,
+		actors: ["Роберт Дауни мл.", "Крис Эванс", "Марк Руффало", "Крис Хемсворт", "Скарлетт Йоханссон", "Джереми Реннер", "Дон Чидл", "Пол Радд", "Бри Ларсон", "Карен Гиллан"]
+		},
+		
+		{
+		name: "Железный человек",
+		year: 2008,
+		actors: ["Роберт Дауни мл.", "Джефф Бриджес", "Гвинет Пэлтроу", "Терренс Ховард"]
+		}
+		
+		]
+		console.log(marvel[2].actors[2]); */
+
+// Значение year для Kitty как достать? Используйте один вариант обращения к объекту в ответе (не стесняйтесь проверять себя в консоли браузера, а в блокноте можно красиво переписать эту же запись!)
+
+
+/* var mixData =
+	[
+		10, "Pussy",
+		["мяу", "мур", "РррР", "ШшшШшш",
+			{
+				name: "Lucky", year: 3, family:
+					[
+						"mama",
+						{ name: "Kitty", year: 3 },
+						"papa"]
+			}
+		]
+	];
+console.log(mixData[2][4].family[1].year);
+ */
+/* var mal =
+{
+	"suggestions": [
+		{
+			"value": "Андрей Мальцев",
+			"unrestricted_value": "Андрей Мальцев",
+			"data": {
+				"surname": "Мальцев",
+				"name": "Андрей",
+				"patronymic": null,
+				"gender": "MALE",
+				"source": null,
+				"qc": "0"
+			}
+		},
+		{
+			"value": "Андрей Малышев",
+			"unrestricted_value": "Андрей Малышев",
+			"data": {
+				"surname": "Малышев",
+				"name": "Андрей",
+				"patronymic": null,
+				"gender": "MALE",
+				"source": null,
+				"qc": "0"
+			}
+		},
+		{
+			"value": "Андрей Малахов",
+			"unrestricted_value": "Андрей Малахов",
+			"data": {
+				"surname": "Малахов",
+				"name": "Андрей",
+				"patronymic": null,
+				"gender": "MALE",
+				"source": null,
+				"qc": "0"
+			}
+		}
+	]
+	
+}
+console.log(mal.suggestions[2].value); */
+
+
+{
+    "email": "mailis@mail.ru",
+    "name": "Rikos",
+    "name1": "",
+    "hobby": "",
+    "surname1": "",
+    "fathername1": "",
+    "cat": "",
+    "dog": "",
+    "parrot": "",
+    "cavy": "",
+    "hamster": "",
+    "squirrel": "",
+    "phone": "",
+    "adres": null,
+    "gender": null,
+    "date_start": {
+        "sec": 0,
+        "usec": 0
+    },
+    "date_updated": {
+        "sec": 1659137281,
+        "usec": 2000
+    },
+    "birthday": {
+        "sec": 0,
+        "usec": 0
+    },
+    "role": [
+        "user"
+    ],
+    "date_register": {
+        "sec": 1659137281,
+        "usec": 2000
+    },
+    "date": "2022-07-30",
+    "by_user": "manager@mail.ru",
+    "companies": [
+        {
+            "name": "Back",
+            "id": 22
+        },
+        {
+            "name": "Front",
+            "id": 21
+        }
+    ],
+    "tasks": [
+        {
+            "name": "Веснушки",
+            "id": 12
+        }
+    ]
+}
